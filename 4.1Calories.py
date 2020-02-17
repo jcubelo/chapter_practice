81ab5936b8bbe9d3365abb665ae5f2e2ec6f3151
jcubelo <60231311+jcubelo@users.noreply.github.com>
2020-02-16 18:02:45 -0600

    Add files via upload

---
+++ 4.1Calories.py
@@ -0,0 +1,10 @@
+minute_list = [10,15,20,25,30]
+for minutes in minute_list:
+    minutes = int(minutes)
+    calories = (4.2*minutes)
+    calories = int(calories)
+    calories = str(calories)
+    minutes = str(minutes)
+    print("You have burned " + calories + " calories in " + minutes + " minutes.")
+
+
